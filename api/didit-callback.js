fbfbb5ab31194b38a0386fba40893291a8903b85
Developer <iphone201988@gmail.com>
2025-06-02 12:15:11 +0530

    initial commit

---
+++ api/didit-callback.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  const data = req.body;
+
+  console.log("✅ DIDIT Webhook Received:");
+  console.log(JSON.stringify(data, null, 2));
+
+  // TODO: You can later save this to a DB or send to email/Slack/etc.
+
+  return res.status(200).json({ message: "Received successfully" });
+}
