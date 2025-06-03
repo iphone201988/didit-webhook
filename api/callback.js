b1d6d4f3915c10460cfaebae4e78c006dc25776d
Developer <iphone201988@gmail.com>
2025-06-03 11:12:15 +0530

    added callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,17 +1,3 @@
 export default function handler(req, res) {
-  // You can optionally read query params here
-  // const { query } = req;
-
-  res.status(200).send(`
-    <html>
-      <head><title>Redirecting...</title></head>
-      <body>
-        <h2>Redirect received</h2>
-        <script>
-          // Optional: redirect back to app via custom scheme or just show status
-          console.log("Callback hit");
-        </script>
-      </body>
-    </html>
-  `);
+  res.status(200).send("Callback page hit successfully!");
 }
